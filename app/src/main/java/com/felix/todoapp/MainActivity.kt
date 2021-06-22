f71467ede50bbe44cdb5edf0636ee7c7df0b1682
fsonic202 <f.oseh@aol.com>
2021-06-22 17:25:38 +0100

    Android Application Login Page Successful Working and Login

--- app/src/main/java/com/felix/todoapp/MainActivity.kt
+++ app/src/main/java/com/felix/todoapp/MainActivity.kt
@@ -1,15 +1,50 @@
  package com.felix.todoapp
 
+import android.app.Application
+import android.app.ApplicationErrorReport
+import android.content.Intent
+import android.content.pm.ApplicationInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-    val Username ="admin"
-    val Password ="admin"
+ class MainActivity : AppCompatActivity() {
+    val validUsername ="admin"
+    val validpassword ="admin"
 
-    private lateinit var Usernameinput:EditText
+    private lateinit var UsernameInput:EditText
+    private lateinit var PasswordInput:EditText
+    private lateinit var SubmitButton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }}
+
+        UsernameInput = findViewById(R.id.UsernameInput)
+        PasswordInput = findViewById(R.id.PasswordInput)
+        SubmitButton = findViewById(R.id.SubmitButton)
+
+
+        SubmitButton.setOnClickListener {
+            val username = UsernameInput.text.toString()
+            val password = PasswordInput.text.toString()
+
+            login(username, password)
+        }
+    }
+    private fun login(username: String, password: String){
+        if(isValidCredentials(username, password)){
+            val intent: Intent = Intent(this,HomeActivity::class.java)
+            intent.putExtra("username",username)
+            startActivity(intent)
+        }else{
+            Toast.makeText(applicationContext, "Please Enter Your Login Details Correctly!", Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+    private fun isValidCredentials(username: String, password: String):Boolean{
+        if(username == validUsername && password == validUsername) return true
+            return false
+    }
+}
